138512de6c604957b670c767336a16dcbeab5507
kopachevsky <kopachevsky@gmail.com>
2013-08-29 17:11:39 +0300

    initial commit

---
+++ app/src/main/java/com/dragan/mdb/MainActivity.java
@@ -0,0 +1,9 @@
+package com.dragan.mdb;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.actionbarsherlock.app.SherlockActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends SherlockActivity  {
+
+}
